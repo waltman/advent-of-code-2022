5ac42dff45fceaa6220cc4f152ab3eff9f1af15d
Walt Mankowski <waltman@pobox.com>
2022-12-01 05:06:53 -0500

    python code for day 1

---
+++ day01/calorie_counting.py
@@ -0,0 +1,16 @@
+from sys import argv
+
+calories = []
+with open(argv[1]) as f:
+    elf = 0
+    for line in f:
+        line = line.rstrip()
+        if line:
+            elf += int(line)
+        else:
+            calories.append(elf)
+            elf = 0
+
+calories.append(elf)
+print('Part 1:', max(calories))
+print('Part 2:', sum(sorted(calories)[-3:]))
