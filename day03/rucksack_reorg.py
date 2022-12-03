ee8d704ac5014dfa0a64059711a8f9f8bdadf55b
Walt Mankowski <waltman@pobox.com>
2022-12-03 07:12:11 -0500

    refactored group_badge()

--- day03/rucksack_reorg.py
+++ day03/rucksack_reorg.py
@@ -10,7 +10,9 @@ def common_item(c1, c2):
     return list({c for c in c1} & {c for c in c2})[0]
 
 def group_badge(rucks):
-    common = {c for c in rucks[0]} & {c for c in rucks[1]} & {c for c in rucks[2]}
+    common = {c for c in rucks[0]}
+    for i in range(1,len(rucks)):
+        common &= {c for c in rucks[i]}
     return list(common)[0]
 
 score = 0
