39401fba26f4978e3da859b6efbe6d2672ca2805
Walt Mankowski <waltman@pobox.com>
2022-12-03 05:53:46 -0500

    python code for day 3 part 2

--- day03/rucksack_reorg.py
+++ day03/rucksack_reorg.py
@@ -9,14 +9,26 @@ def priority(ch):
 def common_item(c1, c2):
     return list({c for c in c1} & {c for c in c2})[0]
 
+def group_badge(rucks):
+    common = {c for c in rucks[0]} & {c for c in rucks[1]} & {c for c in rucks[2]}
+    return list(common)[0]
+
 score = 0
+group_score = 0
 with open(argv[1]) as f:
+    rucks = []
     for line in f:
         line = line.rstrip()
         middle = len(line) // 2
         common = common_item(line[:middle], line[middle:])
         score += priority(common)
+        rucks.append(line)
+        if len(rucks) == 3:
+            badge = group_badge(rucks)
+            group_score += priority(badge)
+            rucks = []
 
 print('Part 1:', score)
+print('Part 2:', group_score)
 
     
