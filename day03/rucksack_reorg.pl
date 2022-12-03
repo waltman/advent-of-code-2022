49b419a19bde38fb34545ae6f47e9c5371de532e
Walt Mankowski <waltman@pobox.com>
2022-12-03 08:45:10 -0500

    perl code for day 3

---
+++ day03/rucksack_reorg.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+use v5.36;
+use Set::Scalar;
+
+sub priority($c) {
+    return 'a' le $c le 'z' ? ord($c) - ord('a') + 1 : ord($c) - ord('A') + 27;
+}
+
+sub common_item($c1, $c2) {
+    my $s1 = Set::Scalar->new(@$c1);
+    my $s2 = Set::Scalar->new(@$c2);
+    return ($s1 * $s2)->[0];
+}
+
+sub group_badge($rucks) {
+    my $common = Set::Scalar->new(@{$rucks->[0]});
+    for my $i (1..$#$rucks) {
+        $common *= Set::Scalar->new(@{$rucks->[$i]});
+    }
+    return $common->[0];
+}
+
+my $score;
+my $group_score;
+my @rucks;
+
+while (<>) {
+    chomp;
+    my @c = split //;
+    my $middle = @c / 2;
+    my $common = common_item([@c[0..$middle-1]], [@c[$middle..$#c]]);
+    $score += priority($common);
+    push @rucks, \@c;
+    if (@rucks == 3) {
+        my $badge = group_badge(\@rucks);
+        $group_score += priority($badge);
+        @rucks = ();
+    }
+}
+
+say "Part 1: ", $score;
+say "Part 2: ", $group_score;
