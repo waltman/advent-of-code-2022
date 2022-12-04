7396d79e2770a7972ab1a3ed6401f1400be5ca5c
Walt Mankowski <waltman@pobox.com>
2022-12-04 07:02:40 -0500

    Python code for day 4 part 2
    also some refactoring

--- day04/camp_cleanup.py
+++ day04/camp_cleanup.py
@@ -1,5 +1,25 @@
 from sys import argv
 
+def contains(elf):
+    if elf[0][0] >= elf[1][0] and elf[0][1] <= elf[1][1]:
+        return True
+    elif elf[1][0] >= elf[0][0] and elf[1][1] <= elf[0][1]:
+        return True
+    else:
+        return False
+
+def overlap(elf):
+    if elf[1][0] <= elf[0][0] <= elf[1][1]:
+        return True
+    elif elf[1][0] <= elf[0][1] <= elf[1][1]:
+        return True
+    elif elf[0][0] <= elf[1][0] <= elf[0][1]:
+        return True
+    elif elf[0][0] <= elf[1][1] <= elf[0][1]:
+        return True
+    else:
+        return False
+
 elves = []
 with open(argv[1]) as f:
     for line in f:
@@ -10,9 +30,12 @@ with open(argv[1]) as f:
         elves.append(pair)
 
 contains_count = 0
+overlap_count = 0
 for elf in elves:
-    if (elf[0][0] >= elf[1][0] and elf[0][1] <= elf[1][1]) or \
-       (elf[1][0] >= elf[0][0] and elf[1][1] <= elf[0][1]):
+    if contains(elf):
         contains_count += 1
+    if overlap(elf):
+        overlap_count += 1
 
 print('Part 1:', contains_count)
+print('Part 2:', overlap_count)
