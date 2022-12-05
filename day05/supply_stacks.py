27f8f78135e82e8670a227a5b5f99b12dbf2b97a
Walt Mankowski <waltman@pobox.com>
2022-12-05 06:16:27 -0500

    python code for day 5 part 1

---
+++ day05/supply_stacks.py
@@ -0,0 +1,34 @@
+from sys import argv
+import re
+
+state = 'parse'
+with open(argv[1]) as f:
+    lines = []
+    for line in f:
+        if state == 'parse':
+            if line[1] == '1':
+                # create stacks
+                num_stacks = int(line[-3])
+                stacks = []
+                for i in range(num_stacks):
+                    stacks.append([])
+                for i in range(len(lines)-1, -1, -1):
+                    for j in range(num_stacks):
+                        idx = 4 * j + 1
+                        if lines[i][idx] != ' ':
+                            stacks[j].append(lines[i][idx])
+                state = 'move'
+            else:
+                lines.append(line)
+        else:
+            if m := re.match('move (\d+) from (\d+) to (\d+)', line):
+                move_cnt = int(m.group(1))
+                move_from = int(m.group(2)) - 1
+                move_to = int(m.group(3)) - 1
+                for i in range(move_cnt):
+                    stacks[move_to].append(stacks[move_from].pop())
+
+print('Part 1:', end='')
+for i in range(num_stacks):
+    print(stacks[i][-1], end='')
+print()
