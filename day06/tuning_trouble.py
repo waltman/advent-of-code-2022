cafb6702193ba19aa608305b67d61d30d8f9b360
Walt Mankowski <waltman@pobox.com>
2022-12-06 05:17:41 -0500

    updated to also solve part 2

--- day06/tuning_trouble.py
+++ day06/tuning_trouble.py
@@ -1,11 +1,12 @@
 from sys import argv
 
-def marker_pos(buf):
-    for i in range(len(buf)-4):
-        if len(set(buf[i:i+4])) == 4:
-            return i+4
+def marker_pos(buf, patlen):
+    for i in range(len(buf)-patlen):
+        if len(set(buf[i:i+patlen])) == patlen:
+            return i+patlen
     return -1
 
 with open(argv[1]) as f:
     for line in f:
-        print('Part 1:', marker_pos(line))
+        print('Part 1:', marker_pos(line, 4))
+        print('Part 2:', marker_pos(line, 14))
