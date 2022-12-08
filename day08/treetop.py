87fdbee152a36565d7ab6e4651e526e9edd642ca
Walt Mankowski (macbook pro) <waltman@pobox.com>
2022-12-08 08:17:40 -0500

    use a single loop for both parts

--- day08/treetop.py
+++ day08/treetop.py
@@ -2,13 +2,13 @@ import sys
 import numpy as np
 
 def is_visible(grid, row, col):
-    if grid[row,col] > max(grid[0:row,col]): # up
+    if grid[row,col] > max(grid[0:row,col], default=0): # up
         return True
-    elif grid[row,col] > max(grid[row+1:,col]): # down
+    elif grid[row,col] > max(grid[row+1:,col]), default=0: # down
         return True
-    elif grid[row,col] > max(grid[row,0:col]): # left
+    elif grid[row,col] > max(grid[row,0:col]), default=0: # left
         return True
-    elif grid[row,col] > max(grid[row,col+1:]): # right
+    elif grid[row,col] > max(grid[row,col+1:]), default=0: # right
         return True
     else:
         return False
@@ -38,19 +38,14 @@ with open(sys.argv[1]) as f:
     grid = np.array([[int(c) for c in line.rstrip()] for line in f])
 
 rows, cols = grid.shape
-num_visible = cols * 2 + 2 * (rows - 2)
-for row in range(1, rows-1):
-    for col in range(1, cols-1):
-        if is_visible(grid, row, col):
-            num_visible += 1
-
-print('Part 1:', num_visible)
-
+num_visible = 0
 max_score = 0
 for row in range(0, rows):
     for col in range(0, cols):
+        if is_visible(grid, row, col):
+            num_visible += 1
         score = scenic_score(grid, row, col)
         max_score = max(max_score, score)
 
+print('Part 1:', num_visible)
 print('Part 2:', max_score)
-
