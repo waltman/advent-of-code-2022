6ac6bfb4511b04815fe2a480699fab88f07507a6
Walt Mankowski <waltman@pobox.com>
2022-12-12 19:34:07 -0500

    simplify the code with a bunch of product iterators

--- day12/hill_climbing_nx.py
+++ day12/hill_climbing_nx.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import networkx as nx
+from itertools import product
 
 def neighbors(row, col, shape):
     if row > 0:
@@ -18,29 +19,22 @@ with open(sys.argv[1]) as f:
 
 # find the start and finish
 rows, cols = grid.shape
-for row in range(rows):
-    for col in range(cols):
-        if grid[row,col] == 'S':
-            start = row,col
-            grid[row,col] = 'a'
-        elif grid[row,col] == 'E':
-            finish = row,col
-            grid[row,col] = 'z'
+for row, col in product(range(rows), range(cols)):
+    if grid[row,col] == 'S':
+        start = row,col
+        grid[row,col] = 'a'
+    elif grid[row,col] == 'E':
+        finish = row,col
+        grid[row,col] = 'z'
 
 # turn the grid into a directed graph
 G = nx.DiGraph()
-for row in range(rows):
-    for col in range(cols):
-        for neighbor in neighbors(row, col, grid.shape):
-            if ord(grid[neighbor[0],neighbor[1]]) - ord(grid[row,col]) <= 1:
-                G.add_edge((row,col), neighbor)
+for row, col in product(range(rows), range(cols)):
+    for neighbor in neighbors(row, col, grid.shape):
+        if ord(grid[neighbor[0],neighbor[1]]) - ord(grid[row,col]) <= 1:
+            G.add_edge((row,col), neighbor)
 print('Part 1:', nx.shortest_path_length(G, start, finish))
 
-# find all the low points
-lows = []
-for row in range(rows):
-    for col in range(cols):
-        if grid[row,col] == 'a':
-            lows.append((row,col))
+lows = [(row,col) for row,col in product(range(rows), range(cols)) if grid[row,col] == 'a']
 dists = nx.shortest_path_length(G, None, finish)
 print('Part 2:', min(map(lambda x: dists.get(x, 1e300), lows)))
