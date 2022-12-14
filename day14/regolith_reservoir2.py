d6fcbc5bb9ec3f5e5412bed9b292f7d818eae219
Walt Mankowski <waltman@pobox.com>
2022-12-14 08:49:46 -0500

    made a few tweaks to my part 1 code to solve part 2

---
+++ day14/regolith_reservoir2.py
@@ -0,0 +1,61 @@
+from sys import argv
+import numpy as np
+from itertools import product
+
+def print_grid(grid, max_row, min_col, max_col):
+    for row in range(max_row+1):
+        for col in range(min_col, max_col+1):
+            print(grid[row,col], end='')
+        print()
+
+grid = np.array([['.' for _ in range(1000)] for _ in range(170)])
+min_col = 1e300
+max_col = -1e300
+max_row = -1e300
+with open(argv[1]) as f:
+    for line in f:
+        points = line.rstrip().split(' -> ')
+        for i in range(len(points)):
+            col, row = [int(x) for x in points[i].split(',')]
+            min_col = min(min_col, col)
+            max_col = max(max_col, col)
+            max_row = max(max_row, row)
+            points[i] = (row, col)
+        for i in range(1, len(points)):
+            row_range = range(min(points[i-1][0], points[i][0]), max(points[i-1][0], points[i][0])+1)
+            col_range = range(min(points[i-1][1], points[i][1]), max(points[i-1][1], points[i][1])+1)
+            grid[row_range,col_range] = '#'
+
+max_row += 2
+grid[max_row,:] = '#'
+
+print_grid(grid, max_row, min_col, max_col)
+
+sand = 0
+while True:
+    row = 0
+    col = 500
+    if grid[row,col] != '.':
+        break
+    while True:
+        if row > max_row:
+            break
+        if grid[row+1,col] == '.':
+            row += 1
+        elif grid[row+1,col-1] == '.':
+            row += 1
+            col -= 1
+        elif grid[row+1,col+1] == '.':
+            row += 1
+            col += 1
+        else:
+            grid[row,col] = 'o'
+            sand += 1
+            max_col = max(max_col, col)
+            min_col = min(min_col, col)
+            break
+    if row > max_row:
+        break
+
+print_grid(grid, max_row, min_col, max_col)
+print('Part 2:', sand)
