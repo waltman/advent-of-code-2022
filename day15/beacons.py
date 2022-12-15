c23d4e4760b56ee3876ffa47a9f71d3def91da6e
Walt Mankowski <waltman@pobox.com>
2022-12-15 15:52:20 -0500

    wip for day 15 part 1
    Parse the input into a class.
    
    (I'm not sure how to solve the rest of it yet, but at least I've got
    this done!)

---
+++ day15/beacons.py
@@ -0,0 +1,21 @@
+from sys import argv
+import re
+
+class Sensor:
+    def __init__(self, srow, scol, brow, bcol):
+        self.srow = int(srow)
+        self.scol = int(scol)
+        self.brow = int(brow)
+        self.bcol = int(bcol)
+
+    def __repr__(self):
+        return f'sensor:({self.srow},{self.scol}) beacon:({self.brow},{self.bcol})'
+
+sensors = []
+with open(argv[1]) as f:
+    for line in f:
+        m = re.search('=([\d-]*).*=([\d-]*).*=([\d-]*).*=([\d-]*)', line)
+        sensors.append(Sensor(m.group(1), m.group(2), m.group(3), m.group(4)))
+for sensor in sensors:
+    print(sensor)
+
