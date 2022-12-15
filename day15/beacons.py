3e945cb363c94488de685efb6b60489d8e24b17e
Walt Mankowski <waltman@pobox.com>
2022-12-15 17:09:19 -0500

    python code for day 15 part 1

--- day15/beacons.py
+++ day15/beacons.py
@@ -11,14 +11,48 @@ class Sensor:
     def dist(self):
         return abs(self.srow - self.brow) + abs(self.scol - self.bcol)
 
+    def can_see(self, row):
+        return abs(row - self.srow) <= self.dist()
+
+    def range_on(self, row):
+        view_len = self.dist() - abs(row - self.srow)
+        return range(self.scol-view_len, self.scol+view_len+1)
+
     def __repr__(self):
         return f'sensor:({self.srow},{self.scol}) beacon:({self.brow},{self.bcol}) dist:{self.dist()}'
 
+infile = argv[1]
+target = int(argv[2])
 sensors = []
-with open(argv[1]) as f:
+with open(infile) as f:
     for line in f:
         m = re.search('=([\d-]*).*=([\d-]*).*=([\d-]*).*=([\d-]*)', line)
-        sensors.append(Sensor(m.group(1), m.group(2), m.group(3), m.group(4)))
+        sensors.append(Sensor(m.group(2), m.group(1), m.group(4), m.group(3)))
+
+free_cols = set()
+# find all the cols that all the sensors can see
 for sensor in sensors:
-    print(sensor)
+    if sensor.can_see(target):
+        free_cols |= set(sensor.range_on(target))
+
+# remove and cols that actually contain a beacon
+for sensor in sensors:
+    if sensor.brow == target and sensor.bcol in free_cols:
+#        print('removing', sensor, 'free_cols', free_cols)
+        free_cols.remove(sensor.bcol)
+
+#print(free_cols, len(free_cols))
+print('Part 1:', len(free_cols))
+
+
+
+#for sensor in sensors:
+#    print(sensor)
+
+# print(sensors[6])
+# for row in range(-3, 18):
+#     if sensors[6].can_see(row):
+#         print(row, sensors[6].range_on(row))
+#     else:
+#         print(row, [])
 
