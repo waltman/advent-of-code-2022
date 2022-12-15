3b54af3dbb29213b064b209e5eb5745199e0b3f8
Walt Mankowski <waltman@pobox.com>
2022-12-15 18:23:54 -0500

    added code to solve part 2

--- day15/beacons.py
+++ day15/beacons.py
@@ -1,5 +1,6 @@
 from sys import argv
 import re
+from sympy import Interval, Union
 
 class Sensor:
     def __init__(self, srow, scol, brow, bcol):
@@ -18,11 +19,34 @@ class Sensor:
         view_len = self.dist() - abs(row - self.srow)
         return range(self.scol-view_len, self.scol+view_len+1)
 
+    def start_end_on(self, row):
+        view_len = self.dist() - abs(row - self.srow)
+        return self.scol-view_len, self.scol+view_len
+
     def __repr__(self):
         return f'sensor:({self.srow},{self.scol}) beacon:({self.brow},{self.bcol}) dist:{self.dist()}'
 
+def union(data):
+    """ Union of a list of intervals e.g. [(1,2),(3,4)] """
+    intervals = [Interval(begin, end) for (begin, end) in data]
+    u = Union(*intervals)
+    return u.args[:2] if isinstance(u, Interval) \
+       else u.args
+
+def union2(data):
+    union = []
+    for begin,end in sorted(data):
+        if union and union[-1][1] >= begin - 1:
+            union[-1][1] = max(union[-1][1], end)
+        else:
+            union.append([begin, end])
+    return union
+    
+
+
 infile = argv[1]
 target = int(argv[2])
+range_max = int(argv[3]) + 1
 sensors = []
 with open(infile) as f:
     for line in f:
@@ -44,7 +68,27 @@ for sensor in sensors:
 #print(free_cols, len(free_cols))
 print('Part 1:', len(free_cols))
 
+# for sensor in sensors:
+#     print(sensor)
 
+for row in range(range_max):
+    ranges = []
+    for sensor in sensors:
+        if sensor.can_see(row):
+            ranges.append(sensor.start_end_on(row))
+
+    merged = union2(ranges)
+#    print(row, merged)
+    if len(merged) > 1:
+        print('Part 2', (merged[0][1]+1) * 4000000 + row)
+        break
+    # if type(merged[0]) == Interval:
+    #     print('Part 2', merged[0].end * 4000000 + row)
+    #     break
+    # if free_cols:
+    #     print(row, free_cols)
+    #     print('Part 2', list(free_cols)[0] * 4000000 + row)
+    #     break
 
 #for sensor in sensors:
 #    print(sensor)
