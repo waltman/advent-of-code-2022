b5f334a6a100043b488781a7e94b57c718954e58
Walt Mankowski <waltman@pobox.com>
2022-12-20 06:40:46 -0500

    added draw_cave()

--- day17/pyroclastic.py
+++ day17/pyroclastic.py
@@ -17,9 +17,18 @@ class Jet:
     def __getitem__(self, idx):
         return self.pattern[idx % len(self.pattern)]
 
+def draw_cave(cave):
+    for row in range(cave.shape[0]-1, -1, -1):
+        print('|', end='')
+        for col in range(7):
+            val = '@' if cave[row,col] else '.'
+            print(val, end='')
+        print('|')
+    print('+-------+')
+
 # initialize the rocks
 rocks = []
-rocks.append(np.array([1,1,1,1]))
+rocks.append(np.array([[1,1,1,1]]))
 rocks.append(np.array([[0,1,0],
                        [1,1,1],
                        [0,1,0]]))
@@ -33,6 +42,15 @@ rocks.append(np.array([[1],
 rocks.append(np.array([[1,1],
                        [1,1]]))
 
+# intialize the cave
+HEIGHT = 10
+cave = np.zeros([HEIGHT, 7], dtype=int)
+
 with open(argv[1]) as f:
     pattern = f.read().rstrip()
     jet = Jet(pattern)
+
+cave[0,2:2+rocks[0].shape[1]] = rocks[0]
+print(cave)
+draw_cave(cave)
+
