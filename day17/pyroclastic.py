1679975e65e34a393f1e57bcdf7a531e1957a94f
Walt Mankowski <waltman@pobox.com>
2022-12-20 10:37:04 -0500

    cleaned up my debugging code

--- day17/pyroclastic.py
+++ day17/pyroclastic.py
@@ -34,8 +34,6 @@ def move_lr(cave, rock, delta, row, lcol):
     rcol = lcol + rock.shape[1]
     cave_copy[row:trow+1, lcol+delta:rcol+delta] += rock
     if np.any(cave_copy[row:trow+1, lcol+delta:rcol+delta].flatten() > 1):
-#        cave_copy = copy(cave)
-#        cave_copy[row:brow:-1, lcol:rcol] += rock
         return cave, lcol # hit something so don't move
     else:
         return cave_copy, lcol + delta
@@ -44,12 +42,8 @@ def move_down(cave, rock, row, lcol):
     cave_copy = copy(cave)
     trow = row + rock.shape[0] - 1
     rcol = lcol + rock.shape[1]
-#    cave_copy[row-1:brow-1:-1, lcol:rcol] += rock
     cave_copy[row-1:trow, lcol:rcol] += rock
-#    if np.any(cave_copy[row-1:brow-1:-1, lcol:rcol].flatten() > 1):
     if np.any(cave_copy[row-1:trow, lcol:rcol].flatten() > 1):
-#        cave_copy = copy(cave)
-#        cave_copy[row:brow:-1, lcol:rcol] += rock
         return cave, row # hit something so don't move
     else:
         return cave_copy, row-1
@@ -60,9 +54,6 @@ rocks.append(np.array([[1,1,1,1]]))
 rocks.append(np.array([[0,1,0],
                        [1,1,1],
                        [0,1,0]]))
-# rocks.append(np.array([[0,0,1],
-#                        [0,0,1],
-#                        [1,1,1]]))
 rocks.append(np.array([[1,1,1],
                        [0,0,1],
                        [0,0,1]]))
@@ -79,14 +70,10 @@ cave = np.zeros([HEIGHT+1, 9], dtype=int)
 cave[0,:] = 1
 cave[:,0] = 1
 cave[:,8] = 1
-#print(cave)
 
 with open(argv[1]) as f:
     pattern = f.read().rstrip()
     jet = Jet(pattern)
-
-#cave[0,3:3+rocks[0].shape[1]] = rocks[0]
-#draw_cave(cave)
 
 height = 1
 for rock_idx in range(2022):
@@ -94,20 +81,16 @@ for rock_idx in range(2022):
     lcol = 3
     rcol = lcol + rock.shape[1]-1
     row = height + 3
-#    print('start, row =', row, 'lcol =', lcol)
     while True:
         delta = next(jet)
         cave_copy, lcol = move_lr(cave, rock, delta, row, lcol)
-#        print('move_lr, row =', row, 'lcol =', lcol)
         
         cave_copy, new_row = move_down(cave, rock, row, lcol)
-#        print('move_down, row =', new_row, 'lcol =', lcol)
 
         if new_row == row:
             trow = row + rock.shape[0] - 1
             rcol = lcol + rock.shape[1]
             cave[row:trow+1, lcol:rcol] += rock
-#            draw_cave(cave)
             height = max(height, trow+1)
             break
         else:
