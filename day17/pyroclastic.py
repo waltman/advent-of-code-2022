462598829728aaf14d4947b3f51e9f780a7401a8
Walt Mankowski <waltman@pobox.com>
2022-12-20 06:22:04 -0500

    made Jet class an iterator

--- day17/pyroclastic.py
+++ day17/pyroclastic.py
@@ -4,6 +4,15 @@ import numpy as np
 class Jet:
     def __init__(self, pattern):
         self.pattern = pattern
+        self.idx = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        res = self.pattern[self.idx % len(self.pattern)]
+        self.idx += 1
+        return res
 
     def __getitem__(self, idx):
         return self.pattern[idx % len(self.pattern)]
@@ -13,4 +22,5 @@ with open(argv[1]) as f:
     jet = Jet(pattern)
 
 for i in range(80):
-    print(jet[i], end='')
+    print(next(jet), end='')
+#    print(jet[i], end='')
