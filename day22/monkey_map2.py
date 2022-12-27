7004d69cf0e1473938a223c5105ff77785cece96
Walt Mankowski <waltman@pobox.com>
2022-12-26 19:51:45 -0500

    this solves part 2 for the test input
    POC to make sure I had the logic right
    
    now I need to recalculate everything for the real input

--- day22/monkey_map2.py
+++ day22/monkey_map2.py
@@ -8,6 +8,16 @@ def draw_grid(grid):
             print(grid[row,col], end='')
         print()
 
+def face(grid, row, col):
+    dim = grid.shape[0] // 3
+
+    if 0 <= row < dim:
+        return 1
+    elif dim <= row < dim * 2:
+        return (col // dim) + 2
+    else:
+        return (col // dim) + 3
+
 delta = {
     'R': (0,1),
     'L': (0,-1),
@@ -36,25 +46,63 @@ score = {
     'U': 3,
 }
 
-def first_col(grid, row):
-    for col in range(grid.shape[1]):
-        if grid[row,col] != ' ':
-            return col
+def first_col(grid, row, col):
+    f = face(grid, row, col)
+    print('first_col', row, col, f)
+    if f == 1:
+        offset = row-0
+        return 11-offset, 15, 'L'
+    elif f == 4:
+        offset = row-4
+        return 8, 15-offset, 'D'
+    elif f == 6:
+        offset = row-8
+        return 3-offset, 11, 'L'
 
-def last_col(grid, row):
-    for col in range(grid.shape[1]-1, -1, -1):
-        if grid[row,col] != ' ':
-            return col
+def last_col(grid, row, col):
+    f = face(grid, row, col)
+    print('last_row', row, col, f)
+    if f == 1:
+        offset = row-0
+        return 4, 7-offset, 'D'
+    elif f == 2:
+        offset = row-4
+        return 15-offset, 11, 'U'
+    elif f == 5:
+        offset = row-4
+        return 7, 7-offset, 'U'
 
-def first_row(grid, col):
-    for row in range(grid.shape[0]):
-        if grid[row,col] != ' ':
-            return row
+def first_row(grid, row, col):
+    f = face(grid, row, col)
+    print('first_row', row, col, f)
+    if f == 2:
+        offset = col-0
+        return 11, 11-offset, 'U'
+    elif f == 3:
+        offset =col-4
+        return 11-offset, 8, 'R'
+    elif f == 5:
+        offset = col-8
+        return 7, 3-offset, 'U'
+    elif f == 6:
+        offset = col-12
+        return 7-offset, 0, 'R'
 
-def last_row(grid, col):
-    for row in range(grid.shape[0]-1, -1, -1):
-        if grid[row,col] != ' ':
-            return row
+def last_row(grid, row, col):
+    f = face(grid, row, col)
+    print('last_row', row, col, f)
+    if f == 2:
+        offset = col-0
+        return 0, 11-offset, 'D'
+    elif f == 3:
+        offset = col-4
+        return 7-offset, 8, 'R'
+    elif f == 1:
+        offset = col-8
+        return 4, 3-offset, 'D'
+    elif f == 6:
+        offset = col-12
+        return 7-offset, 12, 'R'
 
 # parse input into an np.array
 with open(argv[1]) as f:
@@ -80,7 +128,7 @@ with open(argv[1]) as f:
 
 facing =  'R'
 row = 0
-col = first_col(grid, row)
+col = 8
 
 # now walk the path
 dist = 0
@@ -90,32 +138,39 @@ for idx in range(len(path) + 1):
         dist = dist * 10 + int(path[idx])
     else:
 #        print(dist, facing)
-        drow, dcol = delta[facing]
         for _ in range(dist):
-            new_row = row + drow
-            if new_row < 0:
-                new_row = last_row(grid, col)
-            elif new_row >= rows:
-                new_row = first_row(grid, col)
-            elif grid[new_row,col] == ' ' and facing == 'U':
-                new_row = last_row(grid, col)
-            elif grid[new_row,col] == ' ' and facing == 'D':
-                new_row = first_row(grid, col)
+            drow, dcol = delta[facing]
+            if drow != 0:
+                new_row = row + drow
+                if new_row < 0:
+                    new_row, new_col, new_facing = last_row(grid, row, col)
+                elif new_row >= rows:
+                    new_row, new_col, new_facing = first_row(grid, row, col)
+                elif grid[new_row,col] == ' ' and facing == 'U':
+                    new_row, new_col, new_facing = last_row(grid, row, col)
+                elif grid[new_row,col] == ' ' and facing == 'D':
+                    new_row, new_col, new_facing = first_row(grid, row, col)
+                else:
+                    new_col, new_facing = col, facing
 
-            new_col = col + dcol
-            if new_col < 0:
-                new_col = last_col(grid, row)
-            elif new_col >= cols:
-                new_col = first_col(grid, row)
-            elif grid[row,new_col] == ' ' and facing == 'L':
-                new_col = last_col(grid, row)
-            elif grid[row,new_col] == ' ' and facing == 'R':
-                new_col = first_col(grid, row)
+            else:
+                new_col = col + dcol
+                if new_col < 0:
+                    new_row, new_col, new_facing = last_col(grid, row, col)
+                elif new_col >= cols:
+                    new_row, new_col, new_facing = first_col(grid, row, col)
+                elif grid[row,new_col] == ' ' and facing == 'L':
+                    new_row, new_col, new_facing = last_col(grid, row, col)
+                elif grid[row,new_col] == ' ' and facing == 'R':
+                    new_row, new_col, new_facing = first_col(grid, row, col)
+                else:
+                    new_row, new_facing = row, facing
 
             if grid[new_row, new_col] == '#':
                 break
             else:
-                row, col = new_row, new_col
+                row, col, facing = new_row, new_col, new_facing
+                print(f'{row=}, {col=}, {facing=}')
 
 #        print(row, col)
 
@@ -128,4 +183,4 @@ for idx in range(len(path) + 1):
             dist = 0
 
 print(row, col)
-print('Part 1:', (row+1) * 1000 + (col+1) * 4 + score[facing])
+print('Part 2:', (row+1) * 1000 + (col+1) * 4 + score[facing])
