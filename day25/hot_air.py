304afa9b46db2bb394fb5cefbcf71c944b031b94
Walt Mankowski <waltman@pobox.com>
2022-12-27 22:36:41 -0500

    python code for day 25

--- day25/hot_air.py
+++ day25/hot_air.py
@@ -16,7 +16,28 @@ def snafu2dec(line):
         pwr *= 5
     return res
 
+def dec2snafu(val):
+    res = ''
+
+    while val:
+        digit = val % 5
+        rest = val // 5
+        if 0 <= digit <= 2:
+            res = str(digit) + res
+        elif digit == 3:
+            res = '=' + res
+            rest += 1
+        else:
+            res = '-' + res
+            rest += 1
+        val = rest
+    return res
+
+tot = 0
 with open(argv[1]) as f:
     for line in f:
         line = line.strip()
-        print(line, snafu2dec(line))
+        print(line, snafu2dec(line), dec2snafu(snafu2dec(line)))
+        tot += snafu2dec(line)
+
+print('Part 1:', dec2snafu(tot))
